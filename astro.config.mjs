425d166102093e37c645b347421cca3bdd5af7c0
Admink <admink@asdata.xyz>
2024-06-24 09:31:18 +0100

    add 404 page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind()]
+  site:'https://www.aseguratte.es',
+  integrations: [vue(), tailwind(), sitemap()]
 });
